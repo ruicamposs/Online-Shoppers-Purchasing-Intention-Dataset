c53ab4beac68340f6a1bdde9ba06aecd59baf508
ruicamposs <ruimiguelfcampos@gmail.com>
2025-07-15 15:42:30 +0100

    02_conversion_by_visitor_type.sql

---
+++ sql/sql/02_conversion_by_visitor_type.sql
@@ -0,0 +1,8 @@
+SELECT 
+  VisitorType,
+  COUNT(*) AS total_sessions,
+  SUM(CASE WHEN Revenue = TRUE THEN 1 ELSE 0 END) AS total_conversions,
+  ROUND(SUM(CASE WHEN Revenue = TRUE THEN 1 ELSE 0 END) * 1.0 / COUNT(*), 4) AS conversion_rate
+FROM stone-poetry-462613-q7.OnlineShoppersPurchasingIntentionDataset.1
+GROUP BY VisitorType
+ORDER BY conversion_rate DESC;

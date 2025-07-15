4a2d83c2b8e64acc1e31f3ca881e4eda1b76157c
ruicamposs <ruimiguelfcampos@gmail.com>
2025-07-15 15:21:26 +0100

    01_overall_conversion_rate.sql

--- sql/01_overall_conversion_rate.sql
+++ sql/01_overall_conversion_rate.sql
@@ -1,5 +1,5 @@
 SELECT 
   COUNT(*) AS total_sessions,
   SUM(CASE WHEN Revenue = TRUE THEN 1 ELSE 0 END) AS total_conversions,
-  ROUND(SUM(CASE WHEN Revenue = TRUE THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 2) AS conversion_rate_pct
+  ROUND(SUM(CASE WHEN Revenue = TRUE THEN 1 ELSE 0 END) * 1.0 / COUNT(*), 4) AS conversion_rate_pct
 FROM stone-poetry-462613-q7.OnlineShoppersPurchasingIntentionDataset.1

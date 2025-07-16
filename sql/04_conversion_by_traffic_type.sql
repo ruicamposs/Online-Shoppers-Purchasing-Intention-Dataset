73171d7450c69563fd5edcda74bfe8e8e55e55fe
ruicamposs <ruimiguelfcampos@gmail.com>
2025-07-16 10:36:56 +0100

    04_conversion_by_traffic_type.sql

--- sql/04_conversion_by_traffic_type.sql
+++ sql/04_conversion_by_traffic_type.sql
@@ -1,9 +1,9 @@
 SELECT 
-  TrafficType,
+  CONCAT('Type ', CAST(TrafficType AS STRING)) AS traffic_type,
   COUNT(*) AS total_sessions,
   SUM(CASE WHEN Revenue = TRUE THEN 1 ELSE 0 END) AS total_conversions,
   ROUND(SUM(CASE WHEN Revenue = TRUE THEN 1 ELSE 0 END) * 1.0 / COUNT(*), 4) AS conversion_rate
 FROM stone-poetry-462613-q7.OnlineShoppersPurchasingIntentionDataset.1
-GROUP BY TrafficType
+GROUP BY traffic_type
 ORDER BY conversion_rate DESC
 LIMIT 10;
